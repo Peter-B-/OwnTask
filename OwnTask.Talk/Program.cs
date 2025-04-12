7e36ee7e7a9e318d3d94f593694da641bec7a657
Peter Butzhammer <peter@butzhammer.de>
2025-04-12 15:48:04 +0200

    Add template for talk

---
+++ OwnTask.Talk/Program.cs
@@ -0,0 +1,20 @@
+﻿
+Task<object> task = CalculateTheAnswer(
+    QuestionOf.Life | QuestionOf.Universe | QuestionOf.Everything
+    );
+
+
+
+#region Implementation
+
+async Task<object> CalculateTheAnswer(QuestionOf question) => 42;
+
+[Flags]
+public enum QuestionOf
+{
+    Life,
+    Universe,
+    Everything
+}
+
+#endregion
